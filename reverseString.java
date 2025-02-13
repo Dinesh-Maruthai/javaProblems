ff17f391c8bf7f2a4e2a2931ad598079b7022651
DineshMaruthai <dineshmaruthai24@gmail.com>
2025-02-13 18:08:47 +0530

    Update and rename reverseString to reverseString.java

--- reverseString.java
+++ reverseString.java
@@ -1,6 +1,6 @@
-import java.sql.SQLOutput;
+
 import java.util.Scanner;
-public class Main {
+public class reverseString {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
